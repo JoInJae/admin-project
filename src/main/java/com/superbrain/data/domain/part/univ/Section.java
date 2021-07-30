9bdca88cd7a159f6ae60c93c18dd0a27d017ec7f
JoInJae <andinjai@naver.com>
2021-07-30 15:36:46 +0900

    commit 인재

--- src/main/java/com/superbrain/data/domain/part/univ/Section.java
+++ src/main/java/com/superbrain/data/domain/part/univ/Section.java
@@ -5,6 +5,8 @@ import com.superbrain.data.domain.base.BaseEntity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import javax.persistence.*;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 @Entity@Table(name = "Section")
 @Getter@NoArgsConstructor
@@ -17,8 +19,15 @@ public class Section extends BaseEntity {
     @Column(name = "section_name", columnDefinition = "VARCHAR(20)", nullable = false)
     private String name;
 
-    @Column(name = "section_name", columnDefinition = "CHAR(1)", nullable = false)
+    @Column(name = "section_kind", columnDefinition = "CHAR(1)", nullable = false)
     @Enumerated(EnumType.STRING)
     private Kind kind;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "section_parent", referencedColumnName = "section_idx")
+    private Section section;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "section")
+    private Set<Section> sections = new LinkedHashSet<>();
 
 }
